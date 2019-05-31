5d92a03708e9bac1e323abfd283f262553684549
Dipta Das <dipta670@gmail.com>
2019-05-31 12:20:49 +0600

    Add processDesign

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Taco;
 
 @Slf4j
 @Controller
@@ -30,7 +32,18 @@ public class DesignTacoController {
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
+
+		model.addAttribute("design", new Taco());
+
 		return "design";
+	}
+
+	@PostMapping
+	public String processDesign(Taco design) {
+		// Save the taco design...
+		// We'll do this in chapter 3
+		log.info("Processing design: " + design);
+		return "redirect:/orders/current";
 	}
 
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
