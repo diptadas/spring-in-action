90752affc41673420d6af1c53d2e7885e5e0c5d5
Dipta Das <dipta670@gmail.com>
2019-06-01 01:53:05 +0600

    Add TacoRepository and OrderRepository

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -14,48 +14,62 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
 
 	private final IngredientRepository ingredientRepo;
+	private TacoRepository designRepo;
 
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository designRepo) {
 		this.ingredientRepo = ingredientRepo;
+		this.designRepo = designRepo;
+	}
+
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
 	}
 
 	@GetMapping
 	public String showDesignForm(Model model) {
 		List<Ingredient> ingredients = new ArrayList<>();
 		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
 		}
 
-		model.addAttribute("design", new Taco());
-
 		return "design";
 	}
 
 	@PostMapping
-	public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
 		if (errors.hasErrors()) {
 			log.error("Processing design errors: " + errors);
 			return "design";
 		}
-		// Save the taco design...
-		// We'll do this in chapter 3
-		log.info("Processing design: " + design);
+		Taco saved = designRepo.save(design);
+		order.addDesign(saved);
 		return "redirect:/orders/current";
 	}
 
