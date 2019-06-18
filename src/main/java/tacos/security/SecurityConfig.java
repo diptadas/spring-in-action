e7b7975b00d500820cdc8a14633a2e2c4e117f97
diptadas <dipta670@gmail.com>
2019-06-19 00:44:21 +0600

    Custom queries for JDBC authentication

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -16,6 +16,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication().dataSource(dataSource);
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from Users where username=?")
+                .authoritiesByUsernameQuery("select username, authority from Users where username=?");
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,13 @@
 drop table if exists users;
 create table users
 (
-    id       bigint auto_increment,
-    username varchar(255),
-    password varchar(255),
-    enabled  boolean
-);
-insert into users(username, password, enabled)
-values ('steve', '{noop}steve', true);
-insert into users(username, password, enabled)
-values ('john', '{noop}john', true);
-
-drop table if exists authorities;
-create table authorities
-(
+    id        bigint auto_increment,
     username  varchar(255),
+    password  varchar(255),
     authority varchar(255),
-    UNIQUE (username, authority)
+    enabled   boolean
 );
-insert into authorities(username, authority)
-values ('steve', 'admin');
-insert into authorities(username, authority)
-values ('john', 'superadmin');
+insert into users(username, password, authority, enabled)
+values ('steve', '{noop}steve', 'admin', true);
+insert into users(username, password, authority, enabled)
+values ('john', '{noop}john', 'superadmin', true);
